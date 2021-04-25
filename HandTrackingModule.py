fb51e43a2818ef304e2369145faace09a981c937
20160083 <20160083@students.htl-perg.ac.at>
2021-04-25 22:43:57 +0200

    updated control

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -66,7 +66,6 @@ def main():
         p_time = c_time
 
         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
         cv2.imshow("Image", img)
         cv2.waitKey(1)
 
